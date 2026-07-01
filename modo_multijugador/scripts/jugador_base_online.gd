1c46a45b7bebc8f2fe2bda5cafbe6b943832d8fd
Yildiraf Barron <newgate861@gmail.com>
2026-07-01 04:17:41 -0500

    feat: solucion de bug visual, sincronizacion de spritsheets en ambas perspectivas(host y cliente)

--- modo_multijugador/scripts/jugador_base_online.gd
+++ modo_multijugador/scripts/jugador_base_online.gd
@@ -6,15 +6,16 @@ class_name JugadorBaseOnline
 var id_red: int = 1 
 @onready var animador: AnimationPlayer = get_node_or_null("AnimationPlayer")
 
+# === NUEVA VARIABLE PARA RED ===
+# Esta variable guardará el nombre de la animación y viajará por internet
+@export var anim_red: String = ""
+
 func _enter_tree() -> void:
-	# MAGIA DE RED 1: Como el Host nombra a este nodo con el ID del jugador,
-	# cada computadora lee el nombre al nacer y se auto-asigna la autoridad correctamente.
 	set_multiplayer_authority(name.to_int())
 
 func _ready() -> void:
 	add_to_group("jugadores_online")
 
-# MAGIA DE RED 2: Esta función viaja por internet. El Host la usa para forzar la posición.
 @rpc("authority", "call_local", "reliable")
 func fijar_posicion_inicial(pos: Vector2) -> void:
 	global_position = pos
@@ -22,9 +23,16 @@ func fijar_posicion_inicial(pos: Vector2) -> void:
 func _physics_process(_delta: float) -> void:
 	# === BARRERA DE RED ===
 	if multiplayer.has_multiplayer_peer() and not is_multiplayer_authority():
+		# EL ESPECTADOR ENTRA AQUÍ:
+		# En lugar de no hacer nada visualmente, lee la variable que viene por red
+		if anim_red != "" and animador:
+			animador.play(anim_red)
+		elif anim_red == "" and animador:
+			animador.stop()
 		return
 	# ==============================
 
+	# EL DUEÑO (AUTORIDAD) EJECUTA ESTO:
 	var direccion = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"): direccion.x += 1
 	if Input.is_action_pressed("ui_left"): direccion.x -= 1
@@ -36,6 +44,7 @@ func _physics_process(_delta: float) -> void:
 		_ejecutar_animacion(direccion)
 	else:
 		velocity = Vector2.ZERO
+		anim_red = "" # Le avisamos a la red que nos detuvimos
 		if animador and animador.is_playing():
 			animador.stop()
 
@@ -46,13 +55,17 @@ func _ejecutar_animacion(dir: Vector2) -> void:
 	if animador == null:
 		return
 		
+	# Guardamos el nombre de la animación en nuestra variable de red
 	if abs(dir.x) > abs(dir.y):
 		if dir.x < 0:
-			animador.play("perfil_izquierdo")
+			anim_red = "perfil_izquierdo"
 		else:
-			animador.play("perfil_derecho")
+			anim_red = "perfil_derecho"
 	else:
 		if dir.y < 0:
-			animador.play("caminar_atras")
+			anim_red = "caminar_atras"
 		else:
-			animador.play("caminar_frente")
+			anim_red = "caminar_frente"
+			
+	# La reproducimos localmente
+	animador.play(anim_red)
